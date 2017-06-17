57de49c177ad0b4490a9be9c7b962de8e5a812df
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-06-17 13:34:09 +0530

    feat: add found/missing postbacks

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Get started button request:
+#
+# curl -X POST -H "Content-Type: application/json" -d '{
+#   "get_started":{
+#     "payload":"GET_STARTED_PAYLOAD"
+#   }
+# }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=PAGE_ACCESS_TOKEN"
 
 @app.route('/webhook', methods=['GET'])
 def verify():
@@ -38,7 +45,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "got it, thanks!")
+                    send_message_quick_reply(sender_id, "I am sorry, I don't understand that. Did you want to report a missing/found person?")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -47,7 +54,18 @@ def webhook():
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+                    sender_id = messaging_event["sender"]["id"]
+
+                    if messaging_event["postback"]["payload"] == "GET_STARTED_PAYLOAD":
+                        send_message_quick_reply(sender_id, "Welcome to Mumbai Amber Alert. Would you like to report a missing person or report that you may have found a missing person?")
+
+
+                    if messaging_event["postback"]["payload"] == 'found':
+                        send_message('That is great! Please contact the nearest police station")
+
+                    if messaging_event["postback"]["payload"] == 'missing':
+                        response_text = "Please tell us more about the person."
+                        send_message(sender_id, response_text)
 
     return "ok", 200
 
@@ -75,9 +93,46 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text,
+            "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Report Missing",
+                    "payload":"missing",
+                    "image_url":"http://petersfantastichats.com/img/red.png"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"I found a person!",
+                    "payload":"found",
+                    "image_url":"http://petersfantastichats.com/img/green.png"
+                  }
+                ]
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+    print(str(message))
     sys.stdout.flush()
 
 
